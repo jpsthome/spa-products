cb5fbc011a5a5b6283802acfbdcb1b0c9935378e
Joao Pedro <joao.thome@unila.edu.br>
2021-09-15 12:22:31 -0300

    adicionando header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule,
     LayoutModule,
@@ -19,6 +20,6 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatListModule,
   ],
-  exports: [FooterComponent],
+  exports: [FooterComponent, HeaderComponent],
 })
 export class CoreModule {}
