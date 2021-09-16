10e5740051b7220a62f0d779427a762f6f28cc93
Joao Pedro <joao.thome@unila.edu.br>
2021-09-16 15:34:02 -0300

    arrumando layout mobile

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,16 +6,15 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'vcs-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
