d05ca543577d8229bad60383a17ff8ce48162b85
Joao Pedro <joao.thome@unila.edu.br>
2021-09-16 14:00:54 -0300

    adicionando formulario de cadastro e edição de cliente e rotas

--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -1,12 +1,10 @@
+import { ClientsRoutingModule } from './clients-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ClientsFormComponent } from './components/clients-form/clients-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ClientsFormComponent],
+  imports: [CommonModule, ClientsRoutingModule],
 })
-export class ClientsModule { }
+export class ClientsModule {}
---
+++ src/app/modules/clients/components/clients-form/clients-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vcs-clients-form',
+  templateUrl: './clients-form.component.html',
+  styleUrls: ['./clients-form.component.scss']
+})
+export class ClientsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
