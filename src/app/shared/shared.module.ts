576c9f633460762968f066631938b267065e70ff
Joao Pedro <joao.thome@unila.edu.br>
2021-09-15 22:15:26 -0300

    criando confirm-delete-dialogo component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
+import { MaterialModule } from './../core/material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ConfirmDeleteDialogComponent } from './confirm-delete-dialog/confirm-delete-dialog.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ConfirmDeleteDialogComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [ConfirmDeleteDialogComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
